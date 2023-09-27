9d3397b8fa8677142a2ad7eff006f3b1a3e554ee
Nasfame <laciferin@gmail.com>
2023-09-27 05:34:31 +0530

    ipfs: create Direcotry
    Took 2 minutes

--- retrieve-from-ipfs/ipfs.mjs
+++ retrieve-from-ipfs/ipfs.mjs
@@ -1,16 +1,36 @@
 import  * as IPFS from 'ipfs-core';
 import * as fs from 'node-fs'
-async function main() {
-    const ipfs = await IPFS.create();
-    const cid = 'QmSXkyM9bkKPCWaDSvViS14nvbNekGQTJTTeNv7JitSeWZ';  // Replace with your CID
 
+
+async function downloadDirectory(ipfs, ipfsPath){
+    const files = await ipfs.ls(ipfsPath);
+
+    for (const file of files) {
+        console.log(file.name);
+    }
+}
+
+async function downloadFile(ipfs,ipfsPath){
+    const files = await ipfs.ls(ipfsPath);
+
+    for (const file of files) {
+        console.log(file.name);
+    }
     const stream = ipfs.cat(cid);
     let data = '';
 
     for await (const chunk of stream) {
         data += chunk.toString()
     }
-    await fs.writeFile('./downloadedFile', data);  // Replace './downloadedFile' with your file path
+    await fs.writeFile('./downloadedFile', data)
+}
+
+async function main() {
+    const ipfs = await IPFS.create();
+    const cid = 'QmSXkyM9bkKPCWaDSvViS14nvbNekGQTJTTeNv7JitSeWZ';  // Replace with your CID
+
+    await downloadDirectory(cid)
+
 }
 
 main().catch(console.error)
